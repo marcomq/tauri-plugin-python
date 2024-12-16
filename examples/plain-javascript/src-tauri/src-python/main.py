5bb3c58a0fe4094a82e01e45ccf5cf8348370e7d
marco.mengelkoch <Mmengelkoch@gmx.de>
2024-12-16 08:33:07 +0100

    readability python

--- examples/plain-javascript/src-tauri/src-python/main.py
+++ examples/plain-javascript/src-tauri/src-python/main.py
@@ -2,9 +2,9 @@
 
 counter = 0
 
-def greet_python(a):
+def greet_python(input):
   global counter
   counter = counter + 1
-  print("received: " + str(a))
+  print("received: " + str(input))
   s = "" if counter < 2 else "s"
-  return f'Hello {a}! You\'ve been greeted {counter} time{s} from Python.'
+  return f'Hello {input}! You\'ve been greeted {counter} time{s} from Python.'
